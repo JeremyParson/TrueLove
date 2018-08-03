7f789accceb5fa70c5cec0127930ca13ecbd5604
Jeremy Parson <jsbparson@gmail.com>
2018-08-03 12:19:09 -0700

    merged websites

---
+++ main.js
@@ -0,0 +1,127 @@
+//This will be used to select a certain account
+let indexNum;
+
+//This will load the input from the url's Parameters
+function getUrlVars() {
+  var vars = {};
+  var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+      vars[key] = value;
+  });
+  indexNum = vars;
+  indexNum = parseInt(indexNum.index);
+  return indexNum
+}
+
+//This will get the index number input from the previous page
+indexNum = getUrlVars();
+
+console.log(indexNum);
+
+AOS.init({
+  duration: 1200,
+})
+
+//Create Info Variables
+  let fullName = data.results[indexNum].name.title.toUpperCase() + "."
+   + data.results[indexNum].name.first + " " + data.results[indexNum].name.last;
+
+  let age = data.results[indexNum].dob.age;
+  let dob = data.results[indexNum].dob.date;
+  let dateInfo = age + " | " + dob;
+
+  let userLocation = data.results[indexNum].location.street + data.results[indexNum].location.city + 
+  data.results[indexNum].location.state;
+
+  let accountInfo = data.results[indexNum].email + data.results[indexNum].login.username + 
+  data.results[indexNum].login.password;
+
+  let accountAge = data.results[indexNum].registered.age;
+  let registrationDate = data.results[indexNum].registered.date;
+//Used yo replace password characters with periods for security reasons
+  function hidePassword(password){
+      let hiddenPass = "";
+    for(let i in password){
+        hiddenPass += "."
+    }
+    return hiddenPass;
+  }
+  //Display Info on page
+  document.getElementById("account-image").src = data.results[indexNum].picture.large;
+  document.getElementById("name").innerText = fullName;
+  document.getElementById("gender").innerHTML = data.results[indexNum].gender;
+  document.getElementById("Email").innerHTML = data.results[indexNum].email;
+  document.getElementById("dob").innerHTML = dateInfo;
+  document.getElementById("Username").innerHTML = data.results[indexNum].login.username;
+  document.getElementById("Password").innerHTML = hidePassword(data.results[indexNum].login.password);
+  document.getElementById("acount-age").innerHTML = accountAge;
+  document.getElementById("registration-date").innerHTML = registrationDate;
+
+  //Set up all event listeners
+  //This call the callback function and will create a form
+  let changeInfo = document.getElementById("Change-account-info");
+  changeInfo.addEventListener('click',function(){
+  
+    let targetDiv = document.getElementById("info-change");
+
+    targetDiv.className = "zoom-out";
+    targetDiv.style.display = "flex";
+    targetDiv.style.flexDirection = "column";
+    targetDiv.style.alignItems = "center";
+
+
+    let newForm = document.createElement('form');
+    newForm.id = "account-info-form";
+
+    let emailText = document.createElement('input');
+    emailText.id = "email-input";
+    emailText.type = "text";
+    emailText.style.display = "block";
+    emailText.placeholder = "Email";
+
+    let Username = document.createElement('input');
+    Username.id = "username-input";
+    Username.type = "text";
+    Username.style.display = "block";
+    Username.placeholder = "Username";
+
+    let Password = document.createElement('input');
+    Password.id = "password-input";
+    Password.type = "text";
+    Password.style.display = "block";
+    Password.placeholder = "Password";
+
+    let submitButton = document.createElement('button');
+    submitButton.appendChild(document.createTextNode('Submit'));
+    submitButton.addEventListener('click', submit);
+
+    newForm.appendChild(emailText);newForm.appendChild(Username);newForm.appendChild(Password);newForm.appendChild(submitButton);
+
+    targetDiv.appendChild(newForm);
+
+  })
+
+  //This will change the user info
+
+  function submit(event){
+    event.preventDefault();
+    let emailInput = document.getElementById("email-input");
+    let usernameInput = document.getElementById("username-input");
+    let passwordInput =document.getElementById("password-input");
+
+    if(document.getElementById("account-info-form") != null){
+    document.getElementById("account-info-form").remove();
+    
+    if(emailInput.value != ""){
+      document.getElementById("Email").innerHTML = emailInput.value;
+    }
+
+    if(usernameInput.value != ""){
+      document.getElementById("Username").innerHTML = usernameInput.value;
+    }
+
+    if(passwordInput.value != ""){
+      document.getElementById("Password").innerHTML = hidePassword(passwordInput.value);
+    }
+
+    }
+  }
---
+++ main2.js
@@ -0,0 +1,83 @@
+for (let i=0; i<data.results.length;i++){ 
+    let nameBox= document.createElement("div"); 
+    nameBox.id = data.results[i].name.first;
+    nameBox.className="namebox"
+    let name = data.results[i].name.first.substring(0,1).toUpperCase()+data.results[i].name.first.substring(1); 
+    let last_name= data.results[i].name.last.substring(0,1).toUpperCase()+data.results[i].name.last.substring(1); 
+    console.log(name); 
+    let imageURL=data.results[i].picture.large
+    nameBox.innerHTML+=name + " "+ last_name; 
+    let imageElement= document.createElement("img"); 
+    imageElement.src= imageURL;
+    nameBox.appendChild(imageElement);
+ 
+    let locations = data.results[i].location.street
+    let locationss= document.createElement("div") 
+    locationss.className = "location"
+    locationss.innerHTML += locations 
+    let locationsss= data.results[i].location.city 
+    locationss.innerHTML+= "<br> " + locationsss 
+    let locationssss=data.results[i].location.state
+    locationss.innerHTML+= '<br>'+ locationssss 
+    nameBox.appendChild(locationss)
+    document.body.appendChild(nameBox);
+    let loveMePlz = document.createElement('button');
+    // loveMePlz.onclick = `"window.location.href=./index2.html?index = ${i}"`;
+    loveMePlz.setAttribute('onclick', `window.location.href="./index2.html?index=${i}"`);
+    loveMePlz.innerText = "Love Me";
+    nameBox.appendChild(loveMePlz);
+ }
+ 
+ 
+ function alertDateMe(){
+    alert("You have found your true love <3");
+ }
+
+
+ let searchButton = document.getElementById("search");
+ searchButton.addEventListener('click', search);
+
+ window.scroll({
+    top: 2500, 
+    left: 0, 
+    behavior: 'smooth' 
+  });
+  
+  // Scroll certain amounts from current position 
+  window.scrollBy({ 
+    top: 100, // could be negative value
+    left: 0, 
+    behavior: 'smooth' 
+  });
+  let target;
+ function search(event){
+    event.preventDefault();    
+      // Scroll to a certain element
+      target = document.getElementById('search-bar').value;
+      if(document.getElementById(target) != null){
+      console.log(target);
+      console.log(document.getElementById(`${target}`).innerHTML);
+      document.getElementById(target).scrollIntoView({ 
+        behavior: 'smooth' 
+      });
+      document.getElementById(target).className = "target";
+    }else{
+
+        console.log("Searching for a location");
+        
+      for(let i in data.results){
+        targetCity = document.getElementById("search-bar").value;
+        target = document.getElementById(data.results[i].name.first);
+          let targetName;
+            if(data.results[i].location.city == targetCity){
+                console.log("found a matching location!");
+                target.remove();
+                $( "#the-chosen-ones" ).prepend( target );
+            }else{
+                console.log("Did not find a matching location");
+            }
+      }
+    }
+ }
+
+ 
